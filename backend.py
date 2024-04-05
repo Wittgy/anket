ef6af207a514461df76cc3474b1d94bd67cbac61
Wittgy <164667615+Wittgy@users.noreply.github.com>
2024-04-05 19:11:57 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
         form_data = request.form
         # Form verilerini konsola ve tarayıcıya yazdırın
         print("Gelen form verileri:", form_data)
         return render_template('submitted.html', form_data=form_data)
+    else:
+        # GET isteği için başka bir işlem yapılabilir
+        return "405 Method Not Allowed"    
 
 if __name__ == '__main__':
     app.run(debug=True)
